5280c0ef21778daec08a9934c56e433b546c4713
miyamae <yu1ro.mya@gmail.com>
2020-01-28 14:48:05 +0900

    update: SignUp FCで書き換え

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import {
   Text,
   View,
@@ -13,79 +13,66 @@ import { styles } from '../styles';
 import Button from '../components/Button';
 import KeyboardDismiss from '../components/KeyboardDismiss';
 
-type State = {
-  name: string;
-  email: string;
-  password: string;
+const SignUp = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <ImageBackground
+      source={require('../../assets/imgs/welcome.png')}
+      style={[t.flex1, t.bgBlack]}
+      imageStyle={[t.flex1, t.opacity50]}
+      blurRadius={5}
+    >
+      <KeyboardDismiss>
+        <SafeAreaView style={[t.flex1, t.justifyCenter]}>
+          <View style={[t.p4]}>
+            <Text style={[t.textWhite, t.text4xl]}>Create Account</Text>
+            <Text style={[t.textWhite, t.textLg]}>Start chatting!</Text>
+          </View>
+          <View style={[t.p4]}>
+            <Text style={[t.textWhite, t.mB2]}>Name</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Full name"
+              placeholderTextColor="gray"
+              onChangeText={name => setName(name)}
+              value={name}
+            />
+          </View>
+          <View style={[t.p4]}>
+            <Text style={[t.textWhite, t.mB2]}>Email</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Email address"
+              placeholderTextColor="gray"
+              onChangeText={email => setEmail(email)}
+              value={email}
+            />
+          </View>
+          <View style={[t.p4]}>
+            <Text style={[t.textWhite, t.mB2]}>Password</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Password"
+              placeholderTextColor="gray"
+              onChangeText={password => setPassword(password)}
+              value={password}
+            />
+          </View>
+          <View style={[t.p4]}>
+            <Button
+              variant="primary"
+              label="Create Account"
+              onPress={() => {}}
+            />
+          </View>
+        </SafeAreaView>
+      </KeyboardDismiss>
+      <KeyboardAvoidingView behavior="padding" />
+    </ImageBackground>
+  );
 };
 
-export default class SignUp extends Component<object, State> {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      name: '',
-      email: '',
-      password: ''
-    };
-  }
-
-  render() {
-    const { name, email, password } = this.state;
-    return (
-      <ImageBackground
-        source={require('../../assets/imgs/welcome.png')}
-        style={[t.flex1, t.bgBlack]}
-        imageStyle={[t.flex1, t.opacity50]}
-        blurRadius={5}
-      >
-        <KeyboardDismiss>
-          <SafeAreaView style={[t.flex1, t.justifyCenter]}>
-            <View style={[t.p4]}>
-              <Text style={[t.textWhite, t.text4xl]}>Create Account</Text>
-              <Text style={[t.textWhite, t.textLg]}>Start chatting!</Text>
-            </View>
-            <View style={[t.p4]}>
-              <Text style={[t.textWhite, t.mB2]}>Name</Text>
-              <TextInput
-                style={styles.input}
-                placeholder="Full name"
-                placeholderTextColor="gray"
-                onChangeText={name => this.setState({ name })}
-                value={name}
-              />
-            </View>
-            <View style={[t.p4]}>
-              <Text style={[t.textWhite, t.mB2]}>Email</Text>
-              <TextInput
-                style={styles.input}
-                placeholder="Email address"
-                placeholderTextColor="gray"
-                onChangeText={email => this.setState({ email })}
-                value={email}
-              />
-            </View>
-            <View style={[t.p4]}>
-              <Text style={[t.textWhite, t.mB2]}>Password</Text>
-              <TextInput
-                style={styles.input}
-                placeholder="Password"
-                placeholderTextColor="gray"
-                onChangeText={password => this.setState({ password })}
-                value={password}
-              />
-            </View>
-            <View style={[t.p4]}>
-              <Button
-                variant="primary"
-                label="Create Account"
-                onPress={() => {}}
-              />
-            </View>
-          </SafeAreaView>
-        </KeyboardDismiss>
-        <KeyboardAvoidingView behavior="padding" />
-      </ImageBackground>
-    );
-  }
-}
+export default SignUp;
