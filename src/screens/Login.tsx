285f2ba10cc8e4c40d3637753d62f4b4f52cd5fd
miyamae <yu1ro.mya@gmail.com>
2020-01-28 14:50:23 +0900

    update: Login FCで書き換え

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import {
   Text,
   View,
@@ -13,63 +13,51 @@ import { styles } from '../styles';
 import Button from '../components/Button';
 import KeyboardDismiss from '../components/KeyboardDismiss';
 
-type State = {
-  email: string;
-  password: string;
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <ImageBackground
+      source={require('../../assets/imgs/welcome.png')}
+      style={[t.flex1, t.bgBlack]}
+      imageStyle={[t.flex1, t.opacity50]}
+      blurRadius={5}
+    >
+      <KeyboardDismiss>
+        <SafeAreaView style={[t.flex1, t.justifyCenter]}>
+          <View style={[t.p4]}>
+            <Text style={[t.textWhite, t.text4xl]}>Login</Text>
+            <Text style={[t.textWhite, t.textLg]}>Welcome back!</Text>
+          </View>
+          <View style={[t.p4]}>
+            <Text style={[t.textWhite, t.mB2]}>Email</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Email address"
+              placeholderTextColor="gray"
+              onChangeText={email => setEmail(email)}
+              value={email}
+            />
+          </View>
+          <View style={[t.p4]}>
+            <Text style={[t.textWhite, t.mB2]}>Password</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Password"
+              placeholderTextColor="gray"
+              onChangeText={password => setPassword(password)}
+              value={password}
+            />
+          </View>
+          <View style={[t.p4]}>
+            <Button variant="primary" label="Login" onPress={() => {}} />
+          </View>
+        </SafeAreaView>
+      </KeyboardDismiss>
+      <KeyboardAvoidingView behavior="padding" />
+    </ImageBackground>
+  );
 };
 
-export default class Login extends Component<object, State> {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      email: '',
-      password: ''
-    };
-  }
-
-  render() {
-    const { email, password } = this.state;
-    return (
-      <ImageBackground
-        source={require('../../assets/imgs/welcome.png')}
-        style={[t.flex1, t.bgBlack]}
-        imageStyle={[t.flex1, t.opacity50]}
-        blurRadius={5}
-      >
-        <KeyboardDismiss>
-          <SafeAreaView style={[t.flex1, t.justifyCenter]}>
-            <View style={[t.p4]}>
-              <Text style={[t.textWhite, t.text4xl]}>Login</Text>
-              <Text style={[t.textWhite, t.textLg]}>Welcome back!</Text>
-            </View>
-            <View style={[t.p4]}>
-              <Text style={[t.textWhite, t.mB2]}>Email</Text>
-              <TextInput
-                style={styles.input}
-                placeholder="Email address"
-                placeholderTextColor="gray"
-                onChangeText={email => this.setState({ email })}
-                value={email}
-              />
-            </View>
-            <View style={[t.p4]}>
-              <Text style={[t.textWhite, t.mB2]}>Password</Text>
-              <TextInput
-                style={styles.input}
-                placeholder="Password"
-                placeholderTextColor="gray"
-                onChangeText={password => this.setState({ password })}
-                value={password}
-              />
-            </View>
-            <View style={[t.p4]}>
-              <Button variant="primary" label="Login" onPress={() => {}} />
-            </View>
-          </SafeAreaView>
-        </KeyboardDismiss>
-        <KeyboardAvoidingView behavior="padding" />
-      </ImageBackground>
-    );
-  }
-}
+export default Login;
