e9fea42e42918cd6fc0e75439bf89752db3ffa9a
miyamae <yu1ro.mya@gmail.com>
2020-01-28 14:56:23 +0900

    fix: KeyboardDismiss

--- src/components/KeyboardDismiss.tsx
+++ src/components/KeyboardDismiss.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Keyboard, TouchableWithoutFeedback } from 'react-native';
 
 const KeyboardDismiss = (props: any) => (
-  <TouchableWithoutFeedback onPress={() => Keyboard.dismiss}>
+  <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
     {props.children}
   </TouchableWithoutFeedback>
 );
